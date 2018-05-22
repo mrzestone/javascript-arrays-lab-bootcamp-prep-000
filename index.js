ee9deba0f3c3a66d2464ba40595b037ea8fd47c7
Bryan Ochoa-Flores <ochoafloresb@yahoo.com>
2018-05-22 20:07:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function destructivelyAppendKitten(name){
 function destructivelyPrependKitten(name){
   return kittens.unshift(name);
 }
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
