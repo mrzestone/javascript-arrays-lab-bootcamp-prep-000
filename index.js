1bf116b2daad2ec24ee62a87ddceb17e38107a3d
Bryan Ochoa-Flores <ochoafloresb@yahoo.com>
2018-05-22 20:05:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = "I don't do much.";
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name){
-  kittens.push(name)
-  return kittens
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.pop(name);
+  return kittens;
 }
